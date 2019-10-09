4647fbaaabd00d0910e8187a191781e6c0e79155
William Aledander <wgalexan@asu.edu>
2019-10-08 17:40:35 -0700

    Import project and add comments

---
+++ src/cse360assign2/AddingMachine.java
@@ -0,0 +1,66 @@
+package cse360assign2;
+/**
+ *William Alexander
+ *Student ID: 1216514637 
+ *Assignment 2
+ *
+ *This file contains the AddingMachine class, AddingMachine constructor,
+ *and 5 methods: get total, add, subtract, to string, and clear. Add, subtract,
+ *and clear all have a return type of void. To string returns a string and
+ *get total returns an integer
+ *
+ * @author William Alexander 
+ * @version 1.0
+ */
+public class AddingMachine {
+	
+	private int total;
+	
+	/**
+	 * Constructor initializes total to 0
+	 */
+	public AddingMachine() {
+		total = 0; // not needed - included for clarity
+	}
+
+	/**
+	 * Returns the total as an integer
+	 * @return total
+	 */
+	public int getTotal() {
+		return 0;
+	}
+	
+	/**
+	 * Adds the value to the total and
+	 * stores the calculation in the history
+	 * @param value
+	 */
+	public void add(int value) {
+		
+	}
+	
+	/**
+	 * Subtracts the value to the total and
+	 * stores the calculation in the history
+	 * @param value
+	 */
+	public void subtract(int value) {
+		
+	}
+	
+	/**
+	 * Returns calculation history as a string
+	 * @return String representation of the class
+	 */
+	public String toString() {
+		return "";
+	}
+	
+	/**
+	 * Clears the history
+	 */
+	public void clear() {
+		
+	}
+}
